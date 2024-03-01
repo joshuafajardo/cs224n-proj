67caa987c0f17d306601321287d7b207f9845924
Joshua Fajardo <jdfajardo@berkeley.edu>
2024-02-29 23:33:32 -0800

    Add testing for augmented statements

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1,9 +1,10 @@
-# trainer.py
+# train_and_evaluate.py
 
 import pandas as pd
 import torch
 import torch.nn as nn
 
+import argparse
 from datetime import datetime
 import pathlib
 from tqdm import tqdm
@@ -14,7 +15,7 @@ from get_activations import ORIGINAL_ACTIVATIONS_DIR, AUGMENTED_ACTIVATIONS_DIR,
 BASE_RESULTS_DIR = pathlib.Path("results")
 BATCH_SIZE = 32
 
-def main():
+def main(dataset: str) -> None:
   session_name = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
   results_dir = BASE_RESULTS_DIR / session_name
   results_dir.mkdir(parents=True, exist_ok=False)
@@ -24,24 +25,69 @@ def main():
   else:
     device = torch.device("cpu")
 
-  train_test_each_topic("original", results_dir, device)
+  match dataset:
+    case "all":
+      train_eval_original(results_dir / "original", device)
+      train_eval_augmented(results_dir / "augmented", device)
+    case "original":
+      train_eval_original(results_dir / "original", device)
+      train_eval_augmented(results_dir / "augmented", device)
+    case "augmented":
+      train_eval_augmented(results_dir / "augmented", device)
+    case _:
+      raise ValueError("Invalid dataset name")
 
 
-def train_test_each_topic(
-    dataset_type: str,
+def train_eval_augmented(
+    results_dir: pathlib.Path,
+    device: torch.device) -> tuple[pd.DataFrame, pd.DataFrame]:
+  train_topics = {}
+  for activation_file in ORIGINAL_ACTIVATIONS_DIR.glob("*.pt"):
+    train_topics[activation_file.stem] = torch.load(activation_file)
+  
+  test_topics = {}
+  for activation_file in AUGMENTED_ACTIVATIONS_DIR.glob("*.pt"):
+    test_topics[activation_file.stem] = torch.load(activation_file)
+  
+  # Note the difference between how we save the train accuracies vs the test
+  # accuracies.
+  train_accuracies = {}
+  for layer in LAYERS_TO_SAVE:
+    train_accuracies[layer] = -1
+
+  test_accuracies = {}
+  for name in test_topics:
+    test_accuracies[name] = {layer: -1 for layer in LAYERS_TO_SAVE}
+  
+  for layer in LAYERS_TO_SAVE:
+    print(f"Layer: {layer}")
+    sample_topic = next(iter(train_topics.values()))
+    input_size = sample_topic["activations"][layer].size(1)
+
+    truth_classifier = TruthClassifier(input_size).to(device)
+
+    train_loader = create_dataloader(train_topics, layer)
+    train_truth_classifier(
+      truth_classifier, train_loader, device)  # TODO: change
+    train_accuracies[layer] = evaluate_truth_classifier(
+      truth_classifier, train_loader, device)
+
+    for name, test_topic in test_topics.items():
+      test_loader = create_dataloader([test_topic], layer)
+      test_accuracies[name][layer] = evaluate_truth_classifier(
+        truth_classifier, test_loader, device)
+
+    torch.save(truth_classifier, results_dir / f"classifier_layer{layer}.pt")
+  
+  save_results(results_dir, train_accuracies, test_accuracies)
+
+
+def train_eval_original(
     results_dir: pathlib.Path,
     device: torch.device) -> tuple[pd.DataFrame, pd.DataFrame]:
   """Mainly used for replicating Table 1 of Azaria and Mitchell's paper."""
-  match dataset_type:
-    case "original":
-      activations_dir = ORIGINAL_ACTIVATIONS_DIR
-    case "augmented":
-      activations_dir = AUGMENTED_ACTIVATIONS_DIR
-    case _:
-      raise ValueError(f"Invalid dataset type: {dataset_type}")
-
   topics = {}
-  for activation_file in activations_dir.glob("*.pt"):
+  for activation_file in ORIGINAL_ACTIVATIONS_DIR.glob("*.pt"):
     topics[activation_file.stem] = torch.load(activation_file)
   
   train_accuracies = {}
@@ -73,10 +119,7 @@ def train_test_each_topic(
       torch.save(truth_classifier,
                  results_dir / f"classifier_{test_topic_name}_layer{layer}.pt")
 
-  train_accuracies_df = pd.DataFrame(train_accuracies)
-  test_accuracies_df = pd.DataFrame(test_accuracies)
-  train_accuracies_df.to_csv(results_dir / "train_accuracies.csv")
-  test_accuracies_df.to_csv(results_dir / "test_accuracies.csv")
+  save_results(results_dir, train_accuracies, test_accuracies)
 
 
 def create_dataloader(topics: list[dict], layer) -> torch.utils.data.Dataset:
@@ -92,7 +135,7 @@ def train_truth_classifier(truth_classifier: TruthClassifier,
                            loader: torch.utils.data.DataLoader,
                            device: torch.device,
                            epochs: int = 5,
-                           learning_rate: float = 0.01) -> None:
+                           learning_rate: float = 0.001) -> None:
   truth_classifier.to(device)
   truth_classifier.train()
   optimizer = torch.optim.Adam(truth_classifier.parameters(), lr=learning_rate)
@@ -130,5 +173,20 @@ def evaluate_truth_classifier(truth_classifier: TruthClassifier,
   return correct / total
 
 
+def save_results(results_dir: pathlib.Path,
+                 train_accuracies: dict,
+                 test_accuracies: dict) -> None:
+  train_accuracies_df = pd.DataFrame(train_accuracies)
+  test_accuracies_df = pd.DataFrame(test_accuracies)
+  train_accuracies_df.to_csv(results_dir / "train_accuracies.csv")
+  test_accuracies_df.to_csv(results_dir / "test_accuracies.csv")
+
+
 if __name__ == "__main__":
-  main()
+  arg_parser = argparse.ArgumentParser(
+    prog="train_and_evaluate.py",
+    description="Train and evaluate various truth classifiers.")
+  arg_parser.add_argument("-d", "--dataset", type=str, default="all",
+                          help="Dataset to use: all, original, or augmented")
+  args = arg_parser.parse_args()
+  main(args.dataset)
