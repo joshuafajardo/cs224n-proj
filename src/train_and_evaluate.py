9517e9689807a66dedae3acc62acaa5701029819
Joshua Fajardo <jdfajardo@berkeley.edu>
2024-03-01 12:38:57 -0800

    prints

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -146,11 +146,12 @@ def train_eval_original(
 
 
 def create_dataloader(topics: list[dict], layer) -> torch.utils.data.Dataset:
+  if len(topics) == 1:
+    print(topics[0])
+    print(topics[0]["activations"])
   inputs = torch.cat([topic["activations"][layer] for topic in topics])
   labels = torch.cat([torch.tensor(topic["label"].values) for topic in topics])
   labels = labels.unsqueeze(1).float()
-  print(inputs.shape)
-  print(labels.shape)
   return torch.utils.data.DataLoader(
     torch.utils.data.TensorDataset(inputs, labels),
     batch_size=BATCH_SIZE, shuffle=True)
