2d0f784dfe5195b2312ef6f49f97a2e2fa7033d7
Joshua Fajardo <jdfajardo@berkeley.edu>
2024-03-13 22:52:56 -0700

    dropout probs

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -222,7 +222,7 @@ def train_eval_augmented(
       print(f"Layer: {layer}")
       input_size = curr_train_topics[0][layer_to_colname(layer)][0].shape[0]
 
-      truth_classifier = TruthClassifier(input_size).to(device)
+      truth_classifier = TruthClassifier(input_size, dropout_prob=0).to(device)
       train_loader = create_dataloader(curr_train_topics, layer)
       train_truth_classifier(
         truth_classifier, train_loader, device)
@@ -282,7 +282,7 @@ def train_eval_original(
     for layer in LAYERS_TO_SAVE:
       print(f"Layer: {layer}")
       input_size = train_topics[0][layer_to_colname(layer)][0].shape[0]
-      truth_classifier = TruthClassifier(input_size).to(device)
+      truth_classifier = TruthClassifier(input_size, dropout_prob=0).to(device)
 
       train_loader = create_dataloader(train_topics, layer)
       train_truth_classifier(truth_classifier, train_loader, device)
